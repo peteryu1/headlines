92ba17801fefa691f3d3075e84085e6ecf158784
peteryu1 <peteryulung@gmail.com>
2020-10-23 14:12:15 +0800

    template directory

--- headlines.py
+++ headlines.py
@@ -1,3 +1,4 @@
+from flask import render_template
 import feedparser
 from flask import Flask
 
@@ -13,14 +14,6 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>Headlines </h1>
-            <b>{0}</b> </ br>
-            <i>{1}</i> </ br>
-            <p>{2}</p> </ br>
-        </body>
-    </html>""".format(first_article.get("title"), first_article.get("published"),first_article.get("summary"))
-
+    return render_template("home.html")
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
